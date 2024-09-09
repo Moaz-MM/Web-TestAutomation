615ef9ec5bb55af7bfe1c131970842d78f8a6ca7
Moaz-MM <moazmetawea@gmail.com>
2024-09-09 15:16:35 +0300

    Add basetest

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,4 +1,37 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
+
 public class BaseTests {
+
+    private WebDriver driver;
+    protected HomePage homePage;
+
+    @BeforeClass
+    public void setUp(){
+        driver = new ChromeDriver(getChromeoptions());
+        goHome();
+        homePage = new HomePage(driver);
+    }
+
+    private void goHome(){
+        String websiteURL = "https://magento.softwaretestingboard.com/";
+        driver.get(websiteURL);
+    }
+
+    private ChromeOptions getChromeoptions(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--start-maximized");
+        return chromeOptions;
+    }
+
+//    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
 }
